5a6ae64056ba59929b72b956b231817d1f574871
Arseni <arsenidudko@mail.ru>
2017-03-25 02:29:48 +0300

    Add Lesson_2

---
+++ Lesson_2/mysite/base/views.py
@@ -0,0 +1,31 @@
+from django.core            import serializers
+from django.shortcuts       import render
+from django.template        import Template, Context
+from django.http            import HttpResponse, JsonResponse
+from django.utils           import version
+
+from base.models            import Player
+
+import json
+import platform
+
+def say_hello(request):
+    msg = 'Hello, world!'
+    return HttpResponse(msg)
+
+def status(request):
+    template_data = {
+        "Django_version": version.get_complete_version(),
+        "Python_version": platform.python_version(),
+    }
+    return render(request, 'status.html', template_data)
+
+
+
+def list_players(request):
+    data = Player.objects.all()
+    result_list = [data.name for data in data]
+    return HttpResponse(json.dumps(result_list, ensure_ascii=False))
+
+    # return JsonResponse( {'list_players':data} )
+    # result_list = list(Player.objects.all().values('name'))
